e8e1cb3cd729708209a26ee2a453a4a3535decb7
Pham Gia Huy <pgiahuy562@gmail.com>
2022-12-07 14:52:38 +0700

    first commit

---
+++ js/models/products.js
@@ -0,0 +1,11 @@
+function Product (pId, pName, pPrice, pScreen, pBackCamera, pFrontCamera,  pImg, pDesc, pType){
+    this.id = pId;
+    this.name = pName;
+    this.price = pPrice;
+    this.screen = pScreen;
+    this.backCamera = pBackCamera;
+    this.frontCamera = pFrontCamera;
+    this.img = pImg;
+    this.desc = pDesc;
+    this.type = pType;
+}
